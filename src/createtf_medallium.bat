cde7b54ea92a95238cd440bfa97e7188a32bea27
Average_Medal_Enjoyer <128150229+AverageMedalEnjoyer@users.noreply.github.com>
2026-08-18 14:59:06 -0500

    rename mod_tf to tf_medallium

---
+++ src/createtf_medallium.bat
@@ -0,0 +1,9 @@
+@echo off
+
+devtools\bin\vpc.exe /checkfiles /tf /define:SOURCESDK +shaders +game /mksln tf_medallium.sln
+
+if errorlevel 1 (
+    echo.
+    echo VPC failed with exit code %ERRORLEVEL%.
+    pause
+)
