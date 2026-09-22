e1e604b58bfe850c00af2fd923519cbcee593566
blackletum <blackletum@hotmail.com>
2026-09-22 15:07:57 -0400

    Fix missing texture on class selection menu

--- src/game/shared/tf/tf_shareddefs.cpp
+++ src/game/shared/tf/tf_shareddefs.cpp
@@ -49,6 +49,7 @@ const char *g_aPlayerClassNames[TF_CLASS_MENU_BUTTONS] =
 	"#TF_Class_Name_Engineer",
 	"#TF_Class_Name_Civilian",
 	"",
+	"",
 	"#TF_Random"
 };
 
@@ -65,6 +66,7 @@ const char *g_aPlayerClassNames_NonLocalized[TF_CLASS_MENU_BUTTONS] =
 	"Spy",
 	"Engineer",
 	"Civilian",
+	"",
 	"",
 	"Random"
 };
@@ -83,6 +85,7 @@ const char *g_aRawPlayerClassNamesShort[TF_CLASS_MENU_BUTTONS] =
 	"engineer",
 	"civilian",
 	"",
+	"",
 	"random"
 };
 
@@ -99,6 +102,7 @@ const char *g_aRawPlayerClassNames[TF_CLASS_MENU_BUTTONS] =
 	"spy",
 	"engineer",
 	"civilian",
+	"",
 	"",
 	"random"
 };
